4a4d05ff34bad924015c00cfaaa9820b8be4cb2c
joaogabriellisboa <lisboajoaobiel18@gmail.com>
2023-04-23 22:10:06 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let abacate = 20
+let mario = "oi"
+
+console.log(abacate);
+
+function trocacor (cor)
+{ let circulo = document.querySelector(".circulo")
+circulo.style.background = cor } 
+
+function trocaimagem (imagem) 
+{ let imagensiphone = document.querySelector(".iphones")
+ imagensiphone.src= imagem }
+
